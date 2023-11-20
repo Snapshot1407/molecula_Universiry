2a5ddc29be4ef05779835af2e495b431ebddb155
Кужбанов Ислям Баяубаевич <83949058+Snapshot1407@users.noreply.github.com>
2023-11-20 22:54:54 +0600

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -70,12 +70,12 @@ int main()
     unsigned seed = static_cast<unsigned>(time(nullptr)); // семя от времени
     mt19937 rng(seed); // сам генератор
     // диапозон возможных координат
-    uniform_int_distribution<int> distribution_i(0, n-1);
-    uniform_int_distribution<int> distribution_j(0, m-1);
-    uniform_int_distribution<int> move_id(0, 3);
-    uniform_int_distribution<int> cyl_bessel_j_id(1, 100);
+    uniform_int_distribution<int> distribution_i(0, n-1); // движение по строка
+    uniform_int_distribution<int> distribution_j(0, m-1); // движение по столбцам
+    uniform_int_distribution<int> move_id(0, 3); // куда пойдет молекула, атом, частица
+    uniform_int_distribution<int> cyl_bessel_j_id(1, 100); // адсорбция,дисобрция или диффузия
 
-// Создание массива N х M, заполненного нулями
+// Создание массива N х M, заполненного нулями, также его вывод
     int array[n][m];
     for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -93,132 +93,77 @@ int main()
     // };
     
     // исправить в 1 цикл
-    for(auto __ = 0; __ < 100; __++)
+    for(auto __ = 0; __ < 100*n*m; __++)
     {
-    // 1 МК шаг
-        for (auto _ = 0; _ < n*m; _++) 
+
+        // генерация координат
+        short int i = distribution_i(rng); 
+        short int j = distribution_j(rng);
+
+        
+        short int cyl_bessel_j = cyl_bessel_j_id(rng);
+        // cout << endl << "случайные индексы " << i << " " << j << endl;
+
+        if (cyl_bessel_j % 50 != 0)
         {
-                // генерация координат
-                short int i = distribution_i(rng); 
-                short int j = distribution_j(rng);
+            short int move = move_id(rng);
+            // ход наверх, проверка
+            if (array[(i + n - 1) % n][j] == 0 && array[i][j] == 1 && move == 0)
+            {
+                // cout << endl<< "Вывод новой позиции по вертикали " << (i+n-1)%m << endl;
+                array[i][j] = 0;
+                array[( i + n - 1 ) % n][j] = 1;
+                // cout << endl << endl;
+                // for(auto k = 0; k < n; k++) cout << array[k][j] << " "; // вывод измененной столбца
+                // cout << endl << "Новый столбец имеет вид: ";
+            }
+            // ход вправо, проверка
+            else if (array[i][(j+m+1)%m] == 0 && array[i][j] == 1 && move == 1)
+            {
+                // cout << endl<< "Вывод новой позиции по горизонтали " << (j+m+1)%m << " " << endl;
+                array[i][j] = 0;
+                array[i][( j + m + 1 ) % m] = 1;
+                // cout << endl << "Новая строка имеет вид: ";
+                // for(auto k = 0; k < m; k++) cout << array[i][k] << " "; // вывод измененной строки
+                // cout << endl << endl;
+                // cout << endl << endl;
+            }
+            // ход вниз, проверка
+            else if (array[(i+n+1)%n][j] == 0 && array[i][j] == 1 && move == 2)
+            {
+                // cout << endl<< "Вывод новой позиции по вертикали " << (i+n-1)%m << " " << endl;
+                array[i][j] = 0;
+                array[(i+n+1)%n][j] = 1;
+                // cout << endl << "Новый столбец имеет вид: ";
+                // for(auto k = 0; k < n; k++) cout << array[k][j] << " "; // вывод измененной столбца
+                // cout << endl << endl;
+                // cout << endl;
 
-                
-                short int cyl_bessel_j = cyl_bessel_j_id(rng);
-                // cout << endl << "случайные индексы " << i << " " << j << endl;
-
-                if (cyl_bessel_j % 50 != 0)
-                {
-                    short int move = move_id(rng);
-                    // ход наверх, проверка
-                    if (array[(i + n - 1) % n][j] == 0 && array[i][j] == 1 && move == 0)
-                    {
-                        // cout << endl<< "Вывод новой позиции по вертикали " << (i+n-1)%m << endl;
-                        array[i][j] = 0;
-                        array[( i + n - 1 ) % n][j] = 1;
-                        // cout << endl << endl;
-                        // for(auto k = 0; k < n; k++) cout << array[k][j] << " "; // вывод измененной столбца
-                        // cout << endl << "Новый столбец имеет вид: ";
-                    }
-                    // ход вправо, проверка
-                    else if (array[i][(j+m+1)%m] == 0 && array[i][j] == 1 && move == 1)
-                    {
-                        // cout << endl<< "Вывод новой позиции по горизонтали " << (j+m+1)%m << " " << endl;
-                        array[i][j] = 0;
-                        array[i][( j + m + 1 ) % m] = 1;
-                        // cout << endl << "Новая строка имеет вид: ";
-                        // for(auto k = 0; k < m; k++) cout << array[i][k] << " "; // вывод измененной строки
-                        // cout << endl << endl;
-                        // cout << endl << endl;
-                    }
-                    // ход вниз, проверка
-                    else if (array[(i+n+1)%n][j] == 0 && array[i][j] == 1 && move == 2)
-                    {
-                        // cout << endl<< "Вывод новой позиции по вертикали " << (i+n-1)%m << " " << endl;
-                        array[i][j] = 0;
-                        array[(i+n+1)%n][j] = 1;
-                        // cout << endl << "Новый столбец имеет вид: ";
-                        // for(auto k = 0; k < n; k++) cout << array[k][j] << " "; // вывод измененной столбца
-                        // cout << endl << endl;
-                        // cout << endl;
-
-                    }
-                    // ход влево, проверка
-                    else if (array[i][(j+m-1)%m] == 0 && array[i][j] == 1 && move == 3)
-                    {
-                        // cout << endl<< "Вывод новой позиции по горизонтали " << (j+m-1)%m << " " << endl;
-                        array[i][j] = 0;
-                        array[i][(j+m-1)%m] = 1;
-                        // cout << endl << "Новая строка имеет вид: ";
-                        // for(auto k = 0; k < m; k++) cout << array[i][k] << " "; // вывод измененной строки
-                        // cout << endl << endl;
-                        // cout << endl << endl;
-                    }
-                }
-                else{
-                    // cyl_bessel_j
-                        // адсорбция
-                        if (array[i][j] == 0)
-                            {
-                            array[i][j] = 1;
-                            }
-                        // дисорбция
-                        else{
-                            array[i][j] = 0;
-                        }
-                    }
-        // fout << n*m << endl;
-        // fout << ++count << endl;
-        // for (auto y = 0; y < n; y++)
-        // {
-        //     for (auto x = 0; x < m; x++)
-        //     {
-        //         if (array[y][x] == 1 )
-        //         {
-        //             fout << "1" << " " << x << " " << y << " 0" << endl;
-        //         }
-                
-        //         else
-        //         {
-        //             fout << "0" << " " << x << " " << y << " 0" << endl;
-        //         }
-        //     }
-        
-        // }
-        
-        
-        
-
-        // // вывод массива после каждого хода
-        // cout << count << " - номер кадра" << endl;
-        // for (auto ii = 0; ii < n; ii++) 
-        // {
-        //     for (auto jj = 0; jj < m; jj++) 
-        //     {
-        //         if (array[ii][jj] == 1 )
-        //         {
-        //             cout << "1" << " " << ii << " " << jj << " 0" << endl;
-        //         }
-                
-        //         else
-        //         {
-        //             cout << "" << " " << ii << " " << jj << " 0" << endl;
-        //         }
-        //     }
-
-        // }
-
+            }
+            // ход влево, проверка
+            else if (array[i][(j+m-1)%m] == 0 && array[i][j] == 1 && move == 3)
+            {
+                // cout << endl<< "Вывод новой позиции по горизонтали " << (j+m-1)%m << " " << endl;
+                array[i][j] = 0;
+                array[i][(j+m-1)%m] = 1;
+                // cout << endl << "Новая строка имеет вид: ";
+                // for(auto k = 0; k < m; k++) cout << array[i][k] << " "; // вывод измененной строки
+                // cout << endl << endl;
+                // cout << endl << endl;
+            }
         }
-    // // вывод массива после 1 МК шага
-    // cout << ++count << " - номер кадра" << endl;
-    // for (auto i = 0; i < n; i++) 
-    // {
-    //     for (auto j = 0; j < m; j++) 
-    //     {
-    //         cout << array[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << endl;
+    else{
+        // cyl_bessel_j
+            // адсорбция
+            if (array[i][j] == 0)
+                {
+                array[i][j] = 1;
+                }
+            // дисорбция
+            else{
+                array[i][j] = 0;
+            }
+        }
     }
 
     
@@ -228,8 +173,6 @@ int main()
     printconsole(array);
 
     printfile(array);
-
-    
     
     cout << "Степень покрытия молекулами поверхности " << n << "х" << m << ": " << Thetta(array) << endl;
     fout.close(); // закрываем файл
